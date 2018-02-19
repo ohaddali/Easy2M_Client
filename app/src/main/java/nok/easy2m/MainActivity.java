28c07474b201d5002b949ebb09cefcf8bf72fc40
ohaddali <ohadda@post.bgu.ac.il>
2018-02-19 18:06:35 +0200

    First Commit

--- app/src/main/java/nok/easy2m/MainActivity.java
+++ app/src/main/java/nok/easy2m/MainActivity.java
@@ -2,12 +2,60 @@ package nok.easy2m;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import nok.easy2m.models.User;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    Button loginBtn;
+    Button registerBtn;
+    TextView usernameText;
+    TextView passwordText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        registerBtn = findViewById(R.id.registerBtn);
+        usernameText = findViewById(R.id.usernameText);
+        passwordText = findViewById(R.id.passwordText);
+        loginBtn.setOnClickListener(this);
+
+
+    }
+
+
+    @Override
+    public void onClick(View view)
+    {
+        if(view.getId() == loginBtn.getId())
+        {
+            String username = usernameText.getText().toString();
+            String password = usernameText.getText().toString();
+            User user =loginStub(username,password);
+            if(user.isLoggedIn()) {
+                if (user.isAdmin())
+                {
+                    //Admin Intent
+                }
+                else
+                {
+                    //Worker Intent
+                }
+            }
+        }
+
+    }
+
+
+
+    private User loginStub(String username, String password)
+    {
+        return new User();
     }
 }
---
+++ app/src/main/java/nok/easy2m/models/User.java
@@ -0,0 +1,50 @@
+package nok.easy2m.models;
+
+/**
+ * Created by pc on 2/19/2018.
+ */
+
+public class User
+{
+
+    private boolean loggedIn;
+    private String username;
+    private boolean admin;
+    private String name;
+
+    public User()
+    {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+}
