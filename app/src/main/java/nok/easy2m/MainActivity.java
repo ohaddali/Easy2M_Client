0dfd59778034aec3b59a8f133b31be4e38194016
ohaddali <ohadda@post.bgu.ac.il>
2018-02-19 18:06:35 +0200

    First Commit

--- app/src/main/java/nok/easy2m/MainActivity.java
+++ app/src/main/java/nok/easy2m/MainActivity.java
@@ -3,14 +3,23 @@ package nok.easy2m;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Pair;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+import nok.easy2m.models.User;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button loginBtn;
+    Button registerBtn;
+    TextView usernameText;
+    TextView passwordText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,5 +39,42 @@ public class MainActivity extends AppCompatActivity {
         Pair<String,Object> param1 = new Pair<>("param1Name" , map);
         Pair<String,Object> param2 = new Pair<>("param2Name" , "s");
         httpConnection.send(serviceName , methodName , responseCallBack , null , param1 , param2);
+
+
+
+        loginBtn = findViewById(R.id.loginBtn);
+        registerBtn = findViewById(R.id.registerBtn);
+        usernameText = findViewById(R.id.usernameText);
+        passwordText = findViewById(R.id.passwordText);
+        loginBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        if(view.getId() == loginBtn.getId())
+        {
+            String username = usernameText.getText().toString();
+            String password = usernameText.getText().toString();
+            User user =loginStub(username,password);
+            if(user.isLoggedIn()) {
+                if (user.isAdmin())
+                {
+                    //Admin Intent
+                }
+                else
+                {
+                    //Worker Intent
+                }
+            }
+        }
+
+    }
+
+
+
+    private User loginStub(String username, String password)
+    {
+        return new User();
     }
 }
