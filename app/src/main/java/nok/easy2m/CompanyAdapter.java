1c12ff96af0ba3f34edb7dc82edc7047ded70c62
ohaddali <ohadda@post.bgu.ac.il>
2018-02-20 19:38:24 +0200

    Company List Activity

---
+++ app/src/main/java/nok/easy2m/CompanyAdapter.java
@@ -0,0 +1,57 @@
+package nok.easy2m;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import nok.easy2m.models.Company;
+
+/**
+ * Created by pc on 2/20/2018.
+ */
+
+public class CompanyAdapter extends ArrayAdapter<Company>
+{
+    private final Activity context;
+    private List<Company> companies;
+    private LayoutInflater inflater;
+
+    public CompanyAdapter(@NonNull Activity context, int resource, @NonNull List<Company> objects) {
+        super(context, resource, objects);
+        this.context= context;
+        this.companies = objects;
+        inflater =  (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Nullable
+    @Override
+    public Company getItem(int position) {
+        return companies.get(position);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
+    {
+        View rowView=inflater.inflate(R.layout.mylist, parent,false);
+        TextView title = rowView.findViewById(R.id.company_name);
+        ImageView logo = rowView.findViewById(R.id.company_logo);
+        Company item = getItem(position);
+        title.setText(item.getName());
+        if(item.getLogoUrl() != null)
+        {
+            //TODO: Load image from Azure Blobs.
+        }
+        return rowView;
+    }
+}
--- app/src/main/java/nok/easy2m/activities/companiesListActivity.java
+++ app/src/main/java/nok/easy2m/activities/companiesListActivity.java
@@ -1,26 +1,35 @@
 package nok.easy2m.activities;
 
+
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
+
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Pair;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import nok.easy2m.CompanyAdapter;
 import nok.easy2m.R;
 import nok.easy2m.communityLayer.CallBack;
 import nok.easy2m.communityLayer.HttpConnection;
 import nok.easy2m.models.Company;
 import nok.easy2m.models.Services;
-
 public class companiesListActivity extends ListActivity
 {
 
@@ -28,6 +37,16 @@ public class companiesListActivity extends ListActivity
     SharedPreferences pref;
     long workerId;
     boolean isAdmin;
+    String[] itemname ={
+            "Safari",
+            "Camera",
+            "Global",
+            "FireFox",
+            "UC Browser",
+            "Android Folder",
+            "VLC Player",
+            "Cold War"
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,14 +55,75 @@ public class companiesListActivity extends ListActivity
         workerId = pref.getLong("userId" , -1);
         isAdmin = pref.getBoolean("admin",false);
         httpConnection = HttpConnection.getInstance(this);
+
         List<Company> companies = new ArrayList<>();
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setActionBar(toolbar);
+
         CallBack<Company[]> respCallBack = (response) ->
         {
             for(Company company : response)
                 companies.add(company);
         };
-        getCompanies(respCallBack);
+//        getCompanies(respCallBack);
+        //@TODO:
+        Company c = new Company();
+        c.setId(1);
+        c.setName("easy2m");
+        c.setOwnerID(1);
+        c.setDescription("Nice Company");
+        c.setLogoUrl(null);
+        companies.add(c);
+        setListAdapter(new CompanyAdapter(this,R.layout.mylist,companies));
+        getListView().setLongClickable(true);
+        getListView().setOnItemLongClickListener((adapterView, view, position, l) ->
+        {
+            Company item = (Company)adapterView.getItemAtPosition(position);
+            AlertDialog alertDialog = new AlertDialog.Builder(companiesListActivity.this).create();
+            alertDialog.setTitle(item.getName());
+
+            alertDialog.setMessage(item.getDescription());
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL,"OK",
+                    (dialog, which) -> dialog.dismiss());
+            alertDialog.show();
+            return true;
+        });
+
     }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id)
+    {
+        //TODO
+        Toast.makeText(getApplicationContext(), "TODO: Go to company activity", Toast.LENGTH_LONG).show();
+        super.onListItemClick(l, v, position, id);
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.back_btn)
+        {
+            Intent i = new Intent(this,MainActivity.class);
+            SharedPreferences.Editor editor = pref.edit();
+            editor.clear();
+            startActivity(i);
+            finish();
+            return true;
+        }
+        return true;
+    }
+
 
     private void getCompanies(CallBack<Company[]> respCallBack)
     {
